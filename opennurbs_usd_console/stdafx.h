20f60305ae9d229ad559d41936d8105a6f5bd20f
Steve Baer <steve@mcneel.com>
2023-02-23 08:00:00 -0800

    console app now working

---
+++ opennurbs_usd_console/stdafx.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "opennurbs/opennurbs.h"
+#undef min
+#undef max
+#include "pxr/pxr.h"
+#include "pxr/usd/sdf/layer.h"
+#include "pxr/usd/usd/stage.h"
+#include "pxr/usd/sdf/path.h"
+#include "pxr/usd/usdGeom/mesh.h"
+#include "pxr/usd/usdGeom/xform.h"
+#include "pxr/usd/usdGeom/sphere.h"
---
+++ opennurbs_usd_console/write_usd.h
@@ -0,0 +1,4 @@
+#pragma once
+
+// Convert an existing 3dm file into a usd file
+bool WriteUSDFile(const wchar_t* rhinoFilename, const wchar_t* usdFilename);
