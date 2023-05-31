66553af0e98d2d46b17245b8d4129025aebc2f6a
Alain <alain@mcneel.com>
2023-05-31 12:22:05 +0200

    fix paths problem when adding files to usdz. Some cleanup

--- UsdShared/ON_Helpers.h
+++ UsdShared/ON_Helpers.h
@@ -8,6 +8,7 @@ class ON_wString;
 namespace ON_Helpers 
 {
   std::string ON_wStringToStdString(const ON_wString& onwstr);
+  ON_wString ON_TextureTYPE_ToString(const ON_Texture::TYPE type);
   ON_wString StringVectorToPath(const std::vector<ON_wString>& names);
   void RotateYUp(ON_Mesh* mesh);
   void RotateGeometryYUp(ON_Geometry* geom);
