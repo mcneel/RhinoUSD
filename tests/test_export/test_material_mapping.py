6e1a13405f6faa296229a479172b685d8169d42b
Callum Sykes <callumsykes@protonmail.com>
2025-07-18 13:49:20 -0700

    test syntax fix

--- tests/test_export/test_material_mapping.py
+++ tests/test_export/test_material_mapping.py
@@ -28,8 +28,8 @@ export_usdz_file()
 # Texture File is inside usdz
 
 usda_data = get_usd_file_text(export_file_usda)
-assert_element_in_usda_file(usda_data, 'Mesh' 'mesh0'):
-assert_property_in_usda_file(usda_data, 'material:binding'):
-assert_property_in_usda_file(usda_data, 'primvars:st1'):
+assert_element_in_usda_file(usda_data, 'Mesh' 'mesh0')
+assert_property_in_usda_file(usda_data, 'material:binding')
+assert_property_in_usda_file(usda_data, 'primvars:st1')
 
 close_file(test_file)
