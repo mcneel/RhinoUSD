a2c4236fc0f5aa2996b524b26859bcbf201a4e54
Callum Sykes <callumsykes@protonmail.com>
2025-07-18 12:53:48 -0700

    Added better checks and asserts

--- tests/lib.py
+++ tests/lib.py
@@ -20,19 +20,54 @@ def get_export_dir():
 
 def export_usd_files():
     
-    export_dir = get_export_dir();
+    export_usd_file();
+    export_usda_file();
+    export_usdc_file();
+    export_usdz_file();
 
+def export_usd_file():
+    
+    export_dir = get_export_dir();
     export_file_usd = os.path.join(export_dir, 'material-mapping.usd')
+
+    RhinoApp.RunScript("selAll", True);
+    RhinoApp.RunScript(f"-export {export_file_usd} _Enter", True)
+
+    # Ensure files exists
+    assertmsg(os.path.exists(export_file_usd), f'File {export_file_usd} was not exported successfully')
+
+def export_usda_file():
+    
+    export_dir = get_export_dir();
     export_file_usda = os.path.join(export_dir, 'material-mapping.usda')
+
+    RhinoApp.RunScript("selAll", True);
+    RhinoApp.RunScript(f"-export {export_file_usda} _Enter", True)
+
+    # Ensure files exists
+    assertmsg(os.path.exists(export_file_usda), f'File {export_file_usda} was not exported successfully')
+
+def export_usdc_file():
+    
+    export_dir = get_export_dir();
     export_file_usdc = os.path.join(export_dir, 'material-mapping.usdc')
+
+    RhinoApp.RunScript("selAll", True);
+    RhinoApp.RunScript(f"-export {export_file_usdc} _Enter", True)
+
+    # Ensure files exists
+    assertmsg(os.path.exists(export_file_usdc), f'File {export_file_usdc} was not exported successfully')
+
+def export_usdz_file():
+    
+    export_dir = get_export_dir();
     export_file_usdz = os.path.join(export_dir, 'material-mapping.usdz')
 
     RhinoApp.RunScript("selAll", True);
-
-    RhinoApp.RunScript(f"-export {export_file_usd} _Enter", True)
-    RhinoApp.RunScript(f"-export {export_file_usda} _Enter", True)
-    RhinoApp.RunScript(f"-export {export_file_usdc} _Enter", True)
     RhinoApp.RunScript(f"-export {export_file_usdz} _Enter", True)
+
+    # Ensure files exists
+    assertmsg(os.path.exists(export_file_usdz), f'File {export_file_usdz} was not exported successfully')
 
 def open_file(path:str):
     RhinoApp.RunScript(f"-open {path}", True);
@@ -40,3 +75,8 @@ def open_file(path:str):
 
 def close_file(path:str):
     RhinoApp.RunScript(f"_-Close \"{path}\" _Enter", True);
+
+def assertmsg(result:bool, msg:str):
+    if not result:
+        print(msg)
+        assert(False)
--- tests/test_export/test_material_mapping.py
+++ tests/test_export/test_material_mapping.py
@@ -7,5 +7,24 @@ from lib import *
 
 test_file = get_test_file('material-mapping.3dm')
 open_file(test_file)
-export_usd_files();
+
+export_dir = get_export_dir()
+texture_file = os.path.join(export_dir, 'mattano blue.png')
+
+export_usd_file()
+
+assertmsg(os.path.exists(texture_file), 'USD texture file is missing')
+os.remove(texture_file)
+
+export_usda_file()
+assertmsg(os.path.exists(texture_file), 'USDa texture file is missing')
+os.remove(texture_file)
+
+export_usdc_file()
+assertmsg(os.path.exists(texture_file), 'USDc texture file is missing')
+os.remove(texture_file)
+
+export_usdz_file()
+# Texture File is inside usdz
+
 close_file(test_file);
